03170cff789ec146aadaa89752143dc586a1eb24
Timon van Waardhuizen <timonvanwaardhuizen@gmail.com>
2016-09-05 14:13:26 +0200

    testv4

--- test/Assets/Script/turtleScript.cs
+++ test/Assets/Script/turtleScript.cs
@@ -11,6 +11,9 @@ public class turtleScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.Translate(0.1f * Time.deltaTime, 0, 0);
+
+		//chainsaw mothafucka
+		transform.Rotate (0,0,30f);
 	
 	}
 }
